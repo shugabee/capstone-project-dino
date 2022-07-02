a052769ce58c292097446300b233ef6160b2dcf1
Shelby Webb <shelbylyn.webb@gmail.com>
2022-07-02 17:50:33 -0600

    added env

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+
+app.listen(4000, () => console.log('we out here on 4000'));
