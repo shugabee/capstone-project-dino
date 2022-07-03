6804122c2299597ca874c0db6c7d6308733c1f99
Shelby Webb <shelbylyn.webb@gmail.com>
2022-07-02 20:03:32 -0600

    added post request

--- server/server.js
+++ server/server.js
@@ -9,5 +9,13 @@ app.use(cors());
 
 
 
+let name = []
+
+app.post('/name', (req, res) => {
+    const { newName } = req.body
+    name.push(newName)
+    res.status(200).send(name)
+});
+
 
 app.listen(4000, () => console.log('we out here on 4000'));
